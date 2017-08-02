8cf0294514c26f615eda4c6d5a731975230722b7
Arturo Morales <Arturo.Morales@insidemedia.onmicrosoft.com>
2017-08-02 16:24:27 +0000

    Practice

---
+++ GroupM.Context.Console/GroupM.Content.App/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupM.Content.App
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var bannedWords = new List<string> { "horrible", "nasty", "bad", "swine" };
+            string content = "The weather in London in August is bad. Is like winter, horrible";
+
+            int badWords = 0;
+
+            foreach (var bannedWord in bannedWords)
+            {
+                if (content.Contains(bannedWord))
+                {
+                    badWords = badWords + 1;
+                }
+            }
+
+            Console.WriteLine("Text:");
+            Console.WriteLine(content);
+            Console.WriteLine("Total negative words: " + badWords);
+
+            Console.WriteLine("Press ANY key to exit.");
+            Console.ReadKey();
+        }
+
+    }
+}
