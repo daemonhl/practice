f8450c94b179e9615ea385b6d50996f757a01cce
Sergey Rodin <sergey_rodin@epam.com>
2017-08-03 17:37:23 +0400

    Added static repository implementations

---
+++ GroupM.Context.Console/GroupM.Content.Persistence/StaticNegativeWordsRepository.cs
@@ -0,0 +1,65 @@
+﻿using GroupM.Content.Persistence.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GroupM.Content.Entities;
+
+namespace GroupM.Content.Persistence
+{
+    public class StaticNegativeWordsRepository : INegativeWordsRepository
+    {
+        private static Dictionary<int, NegativeWord> words;
+        private static int lastId = 0;
+
+        public StaticNegativeWordsRepository()
+        {
+            var badWords = new List<string> { "horrible", "nasty", "bad", "swine" };
+            words = badWords.ToDictionary(key => badWords.IndexOf(key), value => new NegativeWord { Id = badWords.IndexOf(value), Text = value });
+            lastId = badWords.Count - 1;
+        }
+
+        public void Add(NegativeWord entity)
+        {
+            entity.Id = ++lastId;
+
+            words.Add(entity.Id, entity);
+        }
+
+        public void Delete(int id)
+        {
+            if (words.ContainsKey(id))
+            {
+                words[id] = null;
+            }
+        }
+
+        public NegativeWord Get(int id)
+        {
+            return words[id];
+        }
+
+        public IEnumerable<NegativeWord> GetAll()
+        {
+            return words.Values;
+        }
+
+        public void SaveChanges()
+        {
+            // nothing to do
+        }
+
+        public void Update(NegativeWord entity)
+        {
+            if (words.ContainsKey(entity.Id))
+            {
+                words[entity.Id] = entity;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
---
+++ GroupM.Context.Console/GroupM.Content.Persistence/StaticUserTextsRepository.cs
@@ -0,0 +1,65 @@
+﻿using GroupM.Content.Persistence.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GroupM.Content.Entities;
+
+namespace GroupM.Content.Persistence
+{
+    public class StaticUserTextsRepository : IUserTextsRepository
+    {
+        private static Dictionary<int, UserText> texts;
+        private static int lastId = 0;
+
+        public StaticUserTextsRepository()
+        {
+            texts = new Dictionary<int, UserText>() {
+                { 1, new UserText() { Id = 1, Text = "The weather in London in August is bad. Is like winter, horrible" } }
+            };
+        }
+
+        public void Add(UserText entity)
+        {
+            entity.Id = ++lastId;
+
+            texts.Add(entity.Id, entity);
+        }
+
+        public void Delete(int id)
+        {
+            if (texts.ContainsKey(id))
+            {
+                texts[id] = null;
+            }
+        }
+
+        public UserText Get(int id)
+        {
+            return texts[id];
+        }
+
+        public IEnumerable<UserText> GetAll()
+        {
+            return texts.Values;
+        }
+
+        public void SaveChanges()
+        {
+            // nothing to do
+        }
+
+        public void Update(UserText entity)
+        {
+            if (texts.ContainsKey(entity.Id))
+            {
+                texts[entity.Id] = entity;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
