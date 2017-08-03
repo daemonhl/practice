a0982763d18bf1c7798995cc255ecd32dc845229
Sergey Rodin <sergey_rodin@epam.com>
2017-08-03 14:18:33 +0400

    Created unit tests for TextAnalysisService. All the tests are not passing, functionality is not implemented

--- GroupM.Context.Console/GroupM.Content.Domain.Test/TextAnalysisServiceTest.cs
+++ GroupM.Context.Console/GroupM.Content.Domain.Test/TextAnalysisServiceTest.cs
@@ -7,12 +7,13 @@ using NSubstitute;
 using NUnit.Framework;
 using System;
 
-namespace GroupM.Practice.NegativeWords.LogicTests
+namespace GroupM.Content.Domain.Test
 {
     [TestFixture]
     public class TextAnalysisServiceTest
     {
         private IUnityContainer unityContainer;
+        private ITextAnalysisService service;
 
         [OneTimeSetUp]
         public void Init()
@@ -21,6 +22,8 @@ namespace GroupM.Practice.NegativeWords.LogicTests
             var negativeWordsRepository = Substitute.For<INegativeWordsRepository>();
             negativeWordsRepository.Get(Arg.Is(1)).Returns(new NegativeWord() { Id = 1, Text = "bad" });
             negativeWordsRepository.Get(Arg.Is(2)).Returns(new NegativeWord() { Id = 2, Text = "horrible" });
+            negativeWordsRepository.Get(Arg.Is(3)).Returns(new NegativeWord() { Id = 2, Text = "nasty" });
+            negativeWordsRepository.Get(Arg.Is(4)).Returns(new NegativeWord() { Id = 2, Text = "swine" });
 
             // initialize Unity
             unityContainer = new UnityContainer();
@@ -29,18 +32,46 @@ namespace GroupM.Practice.NegativeWords.LogicTests
             unityContainer.RegisterType<ITextAnalysisService, TextAnalysisService>();
         }
 
+        [SetUp]
+        public void SetUpTest()
+        {
+            service = unityContainer.Resolve<ITextAnalysisService>();
+        }
+
         [Test]
-        public void ShouldThrowNotImplementedException()
+        public void TextAnalysisService_ShouldBeFindingNegativeWordsIfTheyArePresent()
         {
             // Arrange
-            var service = unityContainer.Resolve<ITextAnalysisService>();
-            var userText = new UserText() { Id = 1, Text = "Should be throwing NotImplementedException" };
+            var userText = new UserText() { Id = 1, Text = "The weather in London in August is bad.Is like winter, horrible" };
+
+            // Act
+            var result = service.ProcessText(userText);
+
+            // Assert
+            Assert.That(result.TotalNegativeWords, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void TextAnalysisService_ShouldNotBeFindingNegativeWordsIfTheyAreNotPresent()
+        {
+            // Arrange
+            var userText = new UserText() { Id = 1, Text = "The weather in London in August is very nice and warm compared to Alaska" };
+
+            // Act
+            var result = service.ProcessText(userText);
+
+            // Assert
+            Assert.That(result.TotalNegativeWords, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void TextAnalysisService_ShouldThrowNullReferenceWhenNullPassed()
+        {
+            // Arrange
+            UserText nullUserText = null;
 
             // Act & Assert
-            Assert.Throws(typeof(NotImplementedException), () =>
-            {
-                var result = service.ProcessText(userText);
-            });
+            Assert.Throws(typeof(NullReferenceException), () => service.ProcessText(nullUserText));
         }
 
         [OneTimeTearDown]
