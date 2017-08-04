89f7a6d0c7e46edee2e497ad7f57bf7f038972da
Sergey Rodin <sergey_rodin@epam.com>
2017-08-04 11:07:19 +0400

    Repository tests added

---
+++ GroupM.Context.Console/GroupM.Content.Persistence.Test/StaticNegativeWordsRepositoryTest.cs
@@ -0,0 +1,52 @@
+﻿using GroupM.Content.Entities;
+using Microsoft.Practices.Unity;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupM.Content.Persistence.Test
+{
+    [TestFixture]
+    public class StaticNegativeWordsRepositoryTest
+    {
+        private IUnityContainer unityContainer;
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+
+        }
+
+        [Test]
+        public void StaticNegativeWordsRepository_ShouldHavePredefinedData()
+        {
+            // Arrange
+            var repository = new StaticNegativeWordsRepository();
+
+            // Act
+            var predefinedItems = repository.GetAll();
+            var numberOfItems = predefinedItems.Count();
+
+            // Assert
+            Assert.That(numberOfItems, Is.EqualTo(4));
+        }
+
+        [Test]
+        public void StaticNegativeWordsRepository_ShouldReturnById()
+        {
+            // Arrange
+            var repository = new StaticNegativeWordsRepository();
+            var expectedItem = new NegativeWord() { Id = 0, Text = "horrible" };
+
+            // Act
+            var item = repository.Get(0);
+
+            // Assert
+            Assert.IsTrue(expectedItem.Id == item.Id && expectedItem.Text == item.Text);
+        }
+    }
+}
